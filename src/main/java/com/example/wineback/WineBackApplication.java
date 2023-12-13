d2639e6c8b5bfd0b5a005865189350d86b64f519
pochtalon <pochtalon1010@gmail.com>
2023-12-13 09:09:48 +0200

    Created

---
+++ src/main/java/com/example/wineback/WineBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.wineback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wineback/WineBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wineback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
